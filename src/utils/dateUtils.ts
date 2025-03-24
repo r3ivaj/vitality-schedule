10866bba0fe40c41e394de340849c385447bc8b2
Javier Ponce <imr3ivaj@gmail.com>
2025-03-23 20:37:20 -0600

    chore: simplify logic

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,4 +1,4 @@
-import { format, differenceInDays, isSameDay, isAfter } from 'date-fns';
+import { format, isSameDay, isAfter, differenceInWeeks } from 'date-fns';
 import { WorkoutDetails } from '../types/workout';
 import { workoutSchedule } from './workoutSchedule';
 
@@ -20,10 +20,10 @@ export const getWorkoutForDate = (date: Date): WorkoutDetails => {
     
     // Check if date is after or same as March 29th, 2025
     if (isSameDay(date, march29th2025) || isAfter(date, march29th2025)) {
-      const daysSinceMarch29 = differenceInDays(date, march29th2025);
+      const weeksSinceMarch29 = differenceInWeeks(date, march29th2025);
       
       // If it's March 29th or every 3 weeks after that date
-      if (daysSinceMarch29 % 21 === 0) {
+      if (weeksSinceMarch29 % 3 === 0) {
         return { type: 'Funcional', exercise: 'Isometricos' };
       }
     }
